aa8144c44972be4bb19de6719adb4e5d8ef44a83
Jack-405 <156278363+Jack-405@users.noreply.github.com>
2026-03-22 15:49:30 +0800

    user_Supplement

---
+++ user/src/lib.rs
@@ -0,0 +1,44 @@
+#![no_std]
+#![feature(linkage)]
+
+#[macro_use]
+pub mod console;
+mod syscall;
+mod lang_items;
+
+use syscall::*;
+
+#[no_mangle]
+#[link_section = ".text.entry"]
+pub extern "C" fn _start() -> ! {
+    clear_bss();
+    exit(main());
+    panic!("unreachable after sys_exit!");
+}
+
+#[linkage = "weak"]
+#[no_mangle]
+fn main() -> i32 {
+    panic!("Cannot find main!");
+}
+
+pub fn write(fd: usize, buf: &[u8]) -> isize {
+    sys_write(fd, buf)
+}
+
+pub fn exit(exit_code: i32) -> isize {
+    sys_exit(exit_code)
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn start_bss();
+        fn end_bss();
+    }
+
+    unsafe {
+        (start_bss as usize..end_bss as usize).for_each(|addr| {
+            (addr as *mut u8).write_volatile(0);
+        });
+    }
+}
